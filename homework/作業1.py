112b863cad5502d4c9ff99b9778f592101c099f1
曾敬淳 <dg563bfhfd9@gmail.com>
2021-03-08 22:12:11 +0800

    3/8

---
+++ homework/作業1.py
@@ -0,0 +1,13 @@
+import numpy as np
+# a = np.array([1, 2, 3, 4, 5])
+# a
+# print(a)
+# b = np.array((6, 7, 8))
+# print(type(a), type(b))
+
+a=np.arange(0, 21, 1)
+print(a)
+b=np.arange(0, 21, 2)
+print(b)
+c=np.arange(0, 21, 3)
+print(c)
